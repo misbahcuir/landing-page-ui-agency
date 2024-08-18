c6d80eb04cae4aaea13bd0512ab77bcb2f599b1d
Misbah Uddin <misbah.cuir@yahoo.com>
2024-08-18 23:41:39 +0600

    footer done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'prof-1': "url('./Assests/Images/Microsoft.png')",
+      },
     },
     plugins: [],
   }
- 
+}
